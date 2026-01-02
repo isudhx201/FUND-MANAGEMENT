f7949cf61c04baeebb613c2a222647a81e3e0f34
isudhx201 <isurudhananjayax201@gmail.com>
2026-01-03 04:34:37 +0530

    v2

--- src/worker-script.js
+++ src/worker-script.js
@@ -1,6 +1,6 @@
 // Dummy worker script to satisfy Wrangler requirements if necessary
 export default {
-  async fetch(request, env, ctx) {
+  async fetch() {
     return new Response('Not Implemented', { status: 501 });
   },
 };
